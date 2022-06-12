3d819467725d99c834206add0809001ed61957b5
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-06-12 06:13:31 +0000

    Added form to help form and added validation for name input

--- scripts/map.js
+++ scripts/map.js
@@ -67,13 +67,73 @@ btnToggleConsoleDrawer.addEventListener('click', function () {
 })
 
 // Toggle showing of help form when clicking on help icon
-document.querySelector('#icon-help').addEventListener('click', function() {
+let btnHelp = document.querySelector('#icon-help');
+btnHelp.addEventListener('click', function() {
     let helpBox = document.querySelector('.container-help');
-    helpBox.classList.remove('invisible');
+    helpBox.classList.add('slide-up');
+    helpBox.classList.remove('slide-down');
+
+    // Hide help icon
+    btnHelp.classList.add('invisible');
 })
 
 // Toggle hiding of help form when clicking on close icon
 document.querySelector('#icon-close').addEventListener('click', function() {
     let helpBox = document.querySelector('.container-help');
-    helpBox.classList.add('invisible');
+    helpBox.classList.add('slide-down');
+    helpBox.classList.remove('slide-up');
+
+    // Make help icon visible again
+    btnHelp.classList.remove('invisible');
 })
+
+// Function for checking if name is valid (assuming English context only)
+function isValidName(name) {
+    // Convert name to lowercase
+    let nameWords = name.toLowerCase().split(' '); // Split by whitespaces
+
+    // Check each word of name
+    for (let nameWord of nameWords) {
+        // Check each char of word
+        for (let char of nameWord) {
+            // Convert char to ascii code
+            let ascii = char.charCodeAt(0);
+
+            // Alphabets are from (97-122), apostrophe (') is 39, and dash (-) is 45
+            if ((ascii >= 97 && ascii <= 122) || ascii == 39 || ascii == 45) {
+                return true;
+            } 
+            return false;
+        }
+    }
+    
+}
+
+// TODO
+// Function to validate email address
+
+// Validation for help form
+document.querySelector('#btnHelpSubmit').addEventListener('click', function() {
+    // Set up flags
+    let invalidName = false;
+    let invalidEmail = false;
+    let invalidDescription = false;
+
+    // Select error message tags
+    let errorName = document.querySelector('#error-name')
+    let errorEmail = document.querySelector('#error-email')
+    let errorDescription = document.querySelector('#error-description')
+
+    // Valid name if name does not contain any numbers or special characters (except apostrophe ' and dash -)
+    let nameInput = document.querySelector('#txtName').value;
+    if (!isValidName(nameInput)) {
+        invalidName = true;
+        errorName.innerHTML = `Only alphabets and special characters such as ' and - are allowed.`;
+    }
+    else {
+        errorName.innerHTML = ''; // Clear error message if valid name
+    }
+
+    // TODO: validate email
+
+});
