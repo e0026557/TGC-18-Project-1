440a9f4cb8c320fb0157eee483f3dd4946923a7c
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-06-17 03:45:26 +0000

    Moved miscellaneous functions from map-utilities.js to misc-utilities.js

--- scripts/map-utilities.js
+++ scripts/map-utilities.js
@@ -1,8 +1,8 @@
 // --- Global Variables ---
 // Lookup tables to store names of museums and amenities by their unique coordinates
-const museumMarkerNames = {};
-let amenitiesMarkerNames = {}; // Lookup table will be renewed each time a new nearby amenities query is made
-let userMarkerNames = {};
+// const museumMarkerNames = {};
+// let amenitiesMarkerNames = {}; // Lookup table will be renewed each time a new nearby amenities query is made
+// let userMarkerNames = {};
 
 // --- Leaflet marker icons ---
 const museumIcon = L.icon({
@@ -117,88 +117,88 @@ function setLeafletControlPosition() {
     }
 }
 
-// Function to extract key information from HTML description in museum geojson feature
-function getMuseumInfo(html) {
-    // Create an element to store the HTML description for extraction
-    let divElement = document.createElement('div');
-    divElement.innerHTML = html;
+// // Function to extract key information from HTML description in museum geojson feature
+// function getMuseumInfo(html) {
+//     // Create an element to store the HTML description for extraction
+//     let divElement = document.createElement('div');
+//     divElement.innerHTML = html;
 
-    // Each info is stored under 'td' tags in HTML
-    let columns = divElement.querySelectorAll('td');
+//     // Each info is stored under 'td' tags in HTML
+//     let columns = divElement.querySelectorAll('td');
 
-    // Address info is stored in index 0 (Block No.), 3 (Street), 2 (Postal code)
-    let blockNo = columns[0].innerHTML;
-    let street = columns[3].innerHTML;
-    let postalCode = columns[2].innerHTML;
-    let address = `${blockNo} ${street}, Singapore ${postalCode}`.trim(); // Format address to remove leading and trailing whitespaces
+//     // Address info is stored in index 0 (Block No.), 3 (Street), 2 (Postal code)
+//     let blockNo = columns[0].innerHTML;
+//     let street = columns[3].innerHTML;
+//     let postalCode = columns[2].innerHTML;
+//     let address = `${blockNo} ${street}, Singapore ${postalCode}`.trim(); // Format address to remove leading and trailing whitespaces
 
-    let description = columns[5].innerHTML;
+//     let description = columns[5].innerHTML;
 
-    let name = columns[9].innerHTML;
-    let imageUrl = columns[10].innerHTML;
+//     let name = columns[9].innerHTML;
+//     let imageUrl = columns[10].innerHTML;
 
-    // Return museum object
-    return {
-        'name': name,
-        'description': description,
-        'address': address,
-        'imageUrl': imageUrl
-    };
-}
+//     // Return museum object
+//     return {
+//         'name': name,
+//         'description': description,
+//         'address': address,
+//         'imageUrl': imageUrl
+//     };
+// }
 
-// Function to create HTML element for content display
-function createContentDiv(contentObj, option) {
-    // Set up HTML element to insert into marker popup
-    let div = document.createElement('div');
+// // Function to create HTML element for content display
+// function createContentDiv(contentObj, option) {
+//     // Set up HTML element to insert into marker popup
+//     let div = document.createElement('div');
 
-    // Set up content depending on option
-    // Display distance information
-    let distanceContent = '';
-    if (option.displayDistance) {
-        distanceContent = `
-            <span class="marker--distance">
-                Distance: ${contentObj.distance}m
-            </span>
-        `;
-    }
+//     // Set up content depending on option
+//     // Display distance information
+//     let distanceContent = '';
+//     if (option.displayDistance) {
+//         distanceContent = `
+//             <span class="marker--distance">
+//                 Distance: ${contentObj.distance}m
+//             </span>
+//         `;
+//     }
 
-    // -> Concise content for markers
-    if (option.concise) {
-        div.classList.add('container--marker-content');
-        div.innerHTML = `
-            <h3 class="marker--header">${contentObj.name}</h3>
-            ${distanceContent}
-            <div class="marker--container-button">
-                <button class="btn btn-sm btn-start marker--btn">Set as origin</button>
-                <button class="btn btn-sm btn-end marker--btn">Set as destination</button>
-            </div>
-        `;
-    }
-    // -> Detailed content
-    else {
-        div.classList.add('container--content');
-        div.innerHTML = `
-            <img class="content--img img-fluid" src="${contentObj.imageUrl}" alt="Photo of museum" />
-            <h3 class="content--name">${contentObj.name}</h3>
-            <div class="content--container-button">
-                <button class="btn btn-sm btn-start content--btn">Set as origin</button>
-                <button class="btn btn-sm btn-end content--btn">Set as destination</button>
-            </div>
-            <p class="content--description">${contentObj.description}</p>
-            <address class="content--address">${contentObj.address}</address>
-        `;
-    }
+//     // -> Concise content for markers
+//     if (option.concise) {
+//         div.classList.add('container--marker-content');
+//         div.innerHTML = `
+//             <h3 class="marker--header">${contentObj.name}</h3>
+//             ${distanceContent}
+//             <div class="marker--container-button">
+//                 <button class="btn btn-sm btn-start marker--btn">Set as origin</button>
+//                 <button class="btn btn-sm btn-end marker--btn">Set as destination</button>
+//             </div>
+//         `;
+//     }
+//     // -> Detailed content
+//     else {
+//         div.classList.add('container--content');
+//         div.innerHTML = `
+//             <img class="content--img img-fluid" src="${contentObj.imageUrl}" alt="Photo of museum" />
+//             <h3 class="content--name">${contentObj.name}</h3>
+//             <div class="content--container-button">
+//                 <button class="btn btn-sm btn-start content--btn">Set as origin</button>
+//                 <button class="btn btn-sm btn-end content--btn">Set as destination</button>
+//             </div>
+//             <p class="content--description">${contentObj.description}</p>
+//             <address class="content--address">${contentObj.address}</address>
+//         `;
+//     }
 
-    // Add event listener to buttons
-    div.querySelector('.btn-start').addEventListener('click', function () {
-        testSetNavigationPoint(contentObj.coordinates, contentObj.name, 'start');
-    });
-    div.querySelector('.btn-end').addEventListener('click', function () {
-        testSetNavigationPoint(contentObj.coordinates, contentObj.name, 'end');
-    });
+//     // Add event listener to buttons
+//     div.querySelector('.btn-start').addEventListener('click', function () {
+//         testSetNavigationPoint(contentObj.coordinates, contentObj.name, 'start');
+//     });
+//     div.querySelector('.btn-end').addEventListener('click', function () {
+//         testSetNavigationPoint(contentObj.coordinates, contentObj.name, 'end');
+//     });
 
-    return div;
-}
+//     return div;
+// }
 
 // Function to render all museum markers
 async function renderAllMuseumMarkers(museumIcon, markerLayer) {
@@ -717,18 +717,18 @@ async function displayNearbyResult(museum) {
 }
 
 
-// Function to toggle state of toggle buttons for drawers
-function changeToggleBtnState(button, drawer) {
-    // Check state of drawer
-    if (drawer.dataset.expand == 'true') {
-        button.innerHTML = '<i class="fa-solid fa-chevron-down"></i>';
-        drawer.dataset.expand = 'false';
-    }
-    else {
-        button.innerHTML = '<i class="fa-solid fa-chevron-up"></i>';
-        drawer.dataset.expand = 'true';
-    }
-}
+// // Function to toggle state of toggle buttons for drawers
+// function changeToggleBtnState(button, drawer) {
+//     // Check state of drawer
+//     if (drawer.dataset.expand == 'true') {
+//         button.innerHTML = '<i class="fa-solid fa-chevron-down"></i>';
+//         drawer.dataset.expand = 'false';
+//     }
+//     else {
+//         button.innerHTML = '<i class="fa-solid fa-chevron-up"></i>';
+//         drawer.dataset.expand = 'true';
+//     }
+// }
 
 // Function to show selected tab and hide all other tabs
 function showTabContent(tabClassName) {
@@ -749,52 +749,52 @@ function showTabContent(tabClassName) {
     expandConsoleDrawer();
 }
 
+// // Function for buttons to set start/end points
+// function testSetNavigationPoint(latlng, locationName, option) {
+//     if (option == 'start') {
+//         // Reassign value of global variable start point
+//         START_COORDINATES = latlng;
+//         // Populate start input field of navigation form
+//         document.querySelector('#startPoint').value = locationName;
+//     }
+//     else {
+//         // Reassign value of global variable end point
+//         END_COORDINATES = latlng;
+//         // Populate end input field of navigation form
+//         document.querySelector('#endPoint').value = locationName;
+//     }
 
-function testSetNavigationPoint(latlng, locationName, option) {
-    if (option == 'start') {
-        // Reassign value of global variable start point
-        START_COORDINATES = latlng;
-        // Populate start input field of navigation form
-        document.querySelector('#startPoint').value = locationName;
-    }
-    else {
-        // Reassign value of global variable end point
-        END_COORDINATES = latlng;
-        // Populate end input field of navigation form
-        document.querySelector('#endPoint').value = locationName;
-    }
+//     // Show navigation form
+//     showTabContent('tab--navigation');
 
-    // Show navigation form
-    showTabContent('tab--navigation');
+// }
 
-}
+// // Function for buttons to set start/end points
+// // options: 'start' or 'end'
+// function setNavigationPoint(lat, lon, option) {
+//     let coordinates = [lat, lon];
 
-// Function for buttons to set start/end points
-// options: 'start' or 'end'
-function setNavigationPoint(lat, lon, option) {
-    let coordinates = [lat, lon];
+//     // Get name of location by coordinates
+//     let coordString = coordinates.join(',');
+//     let locationName = museumMarkerNames[coordString] || amenitiesMarkerNames[coordString] || userMarkerNames[coordString]; // Get location name from one of the lookup tables 
 
-    // Get name of location by coordinates
-    let coordString = coordinates.join(',');
-    let locationName = museumMarkerNames[coordString] || amenitiesMarkerNames[coordString] || userMarkerNames[coordString]; // Get location name from one of the lookup tables 
+//     if (option == 'start') {
+//         // Reassign value of global variable start point
+//         startCoordinates = coordinates;
+//         // Populate start input field of navigation form
+//         document.querySelector('#startPoint').value = locationName;
+//     }
+//     else {
+//         // Reassign value of global variable end point
+//         endCoordinates = coordinates;
+//         // Populate end input field of navigation form
+//         document.querySelector('#endPoint').value = locationName;
+//     }
 
-    if (option == 'start') {
-        // Reassign value of global variable start point
-        startCoordinates = coordinates;
-        // Populate start input field of navigation form
-        document.querySelector('#startPoint').value = locationName;
-    }
-    else {
-        // Reassign value of global variable end point
-        endCoordinates = coordinates;
-        // Populate end input field of navigation form
-        document.querySelector('#endPoint').value = locationName;
-    }
+//     // Show navigation form
+//     showTabContent('tab--navigation');
 
-    // Show navigation form
-    showTabContent('tab--navigation');
-
-}
+// }
 
 
 // Function to get user's current coordinates
@@ -846,106 +846,106 @@ function getUserLocation() {
     }
 }
 
-// Function to make Bootstrap tab show and hide other tabs
-// -> Bootstrap tabs are implemented as a sub-tab in search console
-function showSubTabContent(tabId, tabContentId) {
-    // Local function to check if tab is active
-    function isActive(tab) {
-        if (tab.classList.contains('active') && tab.classList.contains('show')) {
-            return true;
-        }
-        return false;
-    }
+// // Function to make Bootstrap tab show and hide other tabs
+// // -> Bootstrap tabs are implemented as a sub-tab in search console
+// function showSubTabContent(tabId, tabContentId) {
+//     // Local function to check if tab is active
+//     function isActive(tab) {
+//         if (tab.classList.contains('active') && tab.classList.contains('show')) {
+//             return true;
+//         }
+//         return false;
+//     }
 
-    // Unselect all tabs except for selected tab to be displayed
-    let tabs = document.querySelectorAll('.nav-link');
-    for (let tab of tabs) {
-        // Show selected tab as active
-        if (tab.id == tabId) {
-            // Add active class if tab is not already active
-            if (!tab.classList.contains('active')) {
-                tab.classList.add('active');
-            }
-        }
-        else {
-            tab.classList.remove('active');
-        }
-    }
+//     // Unselect all tabs except for selected tab to be displayed
+//     let tabs = document.querySelectorAll('.nav-link');
+//     for (let tab of tabs) {
+//         // Show selected tab as active
+//         if (tab.id == tabId) {
+//             // Add active class if tab is not already active
+//             if (!tab.classList.contains('active')) {
+//                 tab.classList.add('active');
+//             }
+//         }
+//         else {
+//             tab.classList.remove('active');
+//         }
+//     }
 
-    // Hide all tab contents (except for selected tab content)
-    let tabContents = document.querySelectorAll('.tab-pane');
-    for (let tabContent of tabContents) {
-        if (tabContent.id == tabContentId) {
-            // Show selected tab if it is not being displayed
-            if (!isActive(tabContent)) {
-                tabContent.classList.add('show');
-                tabContent.classList.add('active');
-            }
-        }
-        else {
-            tabContent.classList.remove('show');
-            tabContent.classList.remove('active');
-        }
-    }
-}
-
-
-// Function for checking if name is valid for help form (assuming English context only)
-function isValidName(name) {
-    // Convert name to lowercase
-    let nameWords = name.toLowerCase().split(' '); // Split by whitespaces
-
-    // Check each word of name
-    for (let nameWord of nameWords) {
-        // Check each char of word
-        for (let char of nameWord) {
-            // Convert char to ascii code
-            let ascii = char.charCodeAt(0);
-
-            // Alphabets are from (97-122), apostrophe (') is 39, and dash (-) is 45
-            if ((ascii >= 97 && ascii <= 122) || ascii == 39 || ascii == 45) {
-                return true;
-            }
-            return false;
-        }
-    }
-
-}
-
-// Function to validate email address for help form
-function isValidEmail(email) {
-    // Valid email address if there is name@domain.xxx format 
-    if (!email.includes('@') || !email.includes('.') || email.includes(' ')) {
-        return false;
-    }
-
-    // Check that there is only 1 '@' symbol 
-    // -> also check that there are no invalid special characters listed below
-    let invalidSpecialChars = `"'(),:<>[]\\/\``;
-    let count = 0;
-    for (let char of email) {
-        if (char == '@') {
-            count++;
-        }
-        else if (invalidSpecialChars.includes(char)) {
-            return false;
-        }
-    }
-    if (count > 1) {
-        return false;
-    }
-
-    // Check that there is a '.' after '@'
-    if (email.lastIndexOf('.') < email.indexOf('@')) {
-        return false;
-    }
+//     // Hide all tab contents (except for selected tab content)
+//     let tabContents = document.querySelectorAll('.tab-pane');
+//     for (let tabContent of tabContents) {
+//         if (tabContent.id == tabContentId) {
+//             // Show selected tab if it is not being displayed
+//             if (!isActive(tabContent)) {
+//                 tabContent.classList.add('show');
+//                 tabContent.classList.add('active');
+//             }
+//         }
+//         else {
+//             tabContent.classList.remove('show');
+//             tabContent.classList.remove('active');
+//         }
+//     }
+// }
 
 
-    // Check that email does not end with '.'
-    if (email[email.length - 1] == '.') {
-        return false;
-    }
+// // Function for checking if name is valid for help form (assuming English context only)
+// function isValidName(name) {
+//     // Convert name to lowercase
+//     let nameWords = name.toLowerCase().split(' '); // Split by whitespaces
 
-    // If email passes the above checks, consider it valid 
-    return true;
-}
+//     // Check each word of name
+//     for (let nameWord of nameWords) {
+//         // Check each char of word
+//         for (let char of nameWord) {
+//             // Convert char to ascii code
+//             let ascii = char.charCodeAt(0);
+
+//             // Alphabets are from (97-122), apostrophe (') is 39, and dash (-) is 45
+//             if ((ascii >= 97 && ascii <= 122) || ascii == 39 || ascii == 45) {
+//                 return true;
+//             }
+//             return false;
+//         }
+//     }
+
+// }
+
+// // Function to validate email address for help form
+// function isValidEmail(email) {
+//     // Valid email address if there is name@domain.xxx format 
+//     if (!email.includes('@') || !email.includes('.') || email.includes(' ')) {
+//         return false;
+//     }
+
+//     // Check that there is only 1 '@' symbol 
+//     // -> also check that there are no invalid special characters listed below
+//     let invalidSpecialChars = `"'(),:<>[]\\/\``;
+//     let count = 0;
+//     for (let char of email) {
+//         if (char == '@') {
+//             count++;
+//         }
+//         else if (invalidSpecialChars.includes(char)) {
+//             return false;
+//         }
+//     }
+//     if (count > 1) {
+//         return false;
+//     }
+
+//     // Check that there is a '.' after '@'
+//     if (email.lastIndexOf('.') < email.indexOf('@')) {
+//         return false;
+//     }
+
+
+//     // Check that email does not end with '.'
+//     if (email[email.length - 1] == '.') {
+//         return false;
+//     }
+
+//     // If email passes the above checks, consider it valid 
+//     return true;
+// }
