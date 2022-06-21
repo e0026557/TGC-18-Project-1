f9f3dfacb2002f40a8b2954d68623e63c9dfaf87
Wesley Cheah <wesley.cheah@trentglobal.edu.sg>
2022-06-21 03:06:43 +0000

    Added functions changeButtonText and changeMultipleButtonText in styles-utilities.js and implemented these functions to change tab buttons' texts based on screen width in styles.js

--- scripts/styles-utilities.js
+++ scripts/styles-utilities.js
@@ -7,3 +7,21 @@ function changePlaceholderText(elementId, longText, shortText) {
         document.querySelector(`#${elementId}`).placeholder = longText;
     }
 }
+
+// Function to change button text for mobile screen width and larger screen widths
+function changeButtonText(buttonId, longText, shortText) {
+    let button = document.querySelector(`#${buttonId}`);
+    if (window.innerWidth < 300) {
+        button.innerHTML = shortText;
+    }
+    else {
+        button.innerHTML = longText;
+    }
+}
+
+// Function to change multiple button texts
+function changeMultipleButtonText(buttonArr) {
+    for (let button of buttonArr) {
+        changeButtonText(button.id, button.longText, button.shortText);
+    }
+}
--- scripts/styles.js
+++ scripts/styles.js
@@ -1,5 +1,30 @@
+// Button texts for console drawer tabs
+const tabButtons = [
+     {
+        'id': 'tab-museum',
+        'shortText': `<i class="fa-solid fa-building-columns"></i>`,
+        'longText': `Museum`
+    },
+    {
+        'id': 'tab-weather', 
+        'shortText': `<i class="fa-solid fa-cloud"></i>`,
+        'longText': `Weather`
+    },
+    {
+        'id': 'tab-nearby',
+        'shortText': `<i class="fa-solid fa-tree-city"></i>`,
+        'longText': `Nearby`
+    },
+];
+
+// Change tab button's text depending on screen width
+changeMultipleButtonText(tabButtons);
+
 // Change search bar's placeholder text depending on screen width
 changePlaceholderText('txtSearch', 'Search Museums@SG', 'Search');
+
+
 window.addEventListener('resize', function() {
     changePlaceholderText('txtSearch', 'Search Museums@SG', 'Search ...');
+    changeMultipleButtonText(tabButtons);
 });
